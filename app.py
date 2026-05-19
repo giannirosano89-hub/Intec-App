2ef40f56316bf4c2f932fd84110eb48f7aa23c5f
giannirosano89-hub <giannirosano89@gmail.com>
2026-05-19 08:47:58 +0200

    Update app.py

--- app.py
+++ app.py
@@ -42,7 +42,7 @@ st.markdown(f"""
     </style>
     <div class="logo-outer-container">
         <div class="logo-container">
-            <img src="data:image/png;base64,{logo_negative}" class="logo-light">
+            <img src="data:image/png;base64,{logo_positive}" class="logo-light">
             <img src="data:image/png;base64,{logo_negative}" class="logo-dark">
         </div>
     </div>
